7354400b2a4b745bf494128479cbc29753498fbf
Samohtxx <thwstgyt@gmail.com>
2025-04-10 16:44:27 -0300

    git add teste.py git commit -m "Sua mensagem" git push

--- teste.py
+++ teste.py
@@ -1,2 +1,9 @@
-#mostrar na tela
+# mostrar na tela
 print('Olá, mundo!')
+nome = input('Qual é o seu nome? ')
+
+idade = input('Qual é a dua idade? ')
+
+peso = input('Quanto vc pesa? ')
+
+print(nome, idade, 'anos de idade', peso, 'kg')
