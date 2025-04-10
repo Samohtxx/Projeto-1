36f0a1b38c3f1fe6dbbacad9f79b44347ffe0f19
Samohtxx <thwstgyt@gmail.com>
2025-04-10 16:52:21 -0300

    alteração

--- teste.py
+++ teste.py
@@ -1,8 +1,9 @@
 # mostrar na tela
 print('Olá, mundo!')
+# variável q receberá valores
 nome = input('Qual é o seu nome? ')
 
-idade = input('Qual é a dua idade? ')
+idade = input('Qual é a sua idade? ')
 
 peso = input('Quanto vc pesa? ')
 
